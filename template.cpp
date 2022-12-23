b18437f4dda236106c708925b1e4201eb147b55f
arko <arkajyotisaha891@gmail.com>
2022-12-23 19:24:13 +0530

    starting

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void file_i_o(){
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+}
+//##################################################
+
+class Solution {
+public:
+    int racecar(int target) {
+        return target;
+    }
+};
+
+
+
+
+int main() {
+ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	file_i_o();
+	// START //
+    Solution s;
+    cout<<s.racecar(5);    
+	return 0;
+}
